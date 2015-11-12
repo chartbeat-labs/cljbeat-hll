b733711d917bb9d2f49a70214e010f53416aab2e
Matt Krukowski <matthew@chartbeat.com>
2015-11-12 14:01:43 -0500

    cleanup project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject com.chartbeat.cljbeat/hll "1.0.0"
-  :description "Hll implementation in clojure"
+  :description "Hyperloglog implementation in clojure"
   :url "https://github.com/chartbeat-labs/cljbeat-hll"
   :license {:name "Apache License, Version 2.0"
-                :url "http://www.apache.org/license/LICENSE-2.0.html"}
+            :url "http://www.apache.org/license/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [pandect "0.4.1"]]
